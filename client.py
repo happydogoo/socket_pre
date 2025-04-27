bbf494d96bd84c9e3824b0114de4d782312bc681
happydogoo <149953549+happydogoo@users.noreply.github.com>
2025-04-27 23:43:25 +0800

    Add files via upload

--- client.py
+++ client.py
@@ -14,12 +14,12 @@ def run_client(server_ip, server_port, count=10, timeout=1.0):
     for seq in range(1, count+1):
         # 构造消息：包含序号和发送时间
 
-        send_ts = time.time()#发送时间
+        send_ts = time.time()  # 发送时间
         msg = f"{seq}号PING报文，发送时间：{send_ts}"
         try:
             sock.sendto(msg.encode('utf-8'), addr)
             data, _ = sock.recvfrom(1024)
-            recv_ts = time.time()#收到回复时间
+            recv_ts = time.time()  # 收到回复时间
 
             # 计算RTT
             rtt = (recv_ts - send_ts) * 1000
@@ -27,15 +27,12 @@ def run_client(server_ip, server_port, count=10, timeout=1.0):
             print(f" 从{server_ip}:{server_port}收到报文，序号为{seq}，往返时延为{rtt:.3f} ms，报文信息为：“{reply}”")
         except socket.timeout:
             print(f" 序号为{seq}的报文响应超时")
-    sock.sendto("exit".encode('utf-8'),addr)
-
-        # 每次发送之间可选等待
-        # time.sleep(1)
+    sock.sendto("exit".encode('utf-8'), addr)
 
     sock.close()
 
 if __name__ == '__main__':
-
-    server_ip = "127.0.0.1";
+    # 让用户输入服务器IP地址
+    server_ip = input("请输入服务器IP地址: ")
     server_port = int("12000")
     run_client(server_ip, server_port)
